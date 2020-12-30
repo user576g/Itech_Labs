087dbd0459b2471aef1a94468ae1d5a55c545a13
user576g <user_576@ukr.net>
2020-12-30 23:14:54 +0200

    Sory It's not goof to show

--- Library/upload-file.php
+++
@@ -1,8 +0,0 @@
-    <!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
-<form enctype="multipart/form-data" action="__URL__" method="POST">
-    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-    <!-- Название элемента input определяет имя в массиве $_FILES -->
-    Отправить этот файл: <input name="userfile" type="file" />
-    <input type="submit" value="Отправить файл" />
-</form>
